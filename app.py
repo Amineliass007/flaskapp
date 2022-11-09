dcb07109f0853453cf19d8791518bc1c153cc43d
Amineliass007 <115976127+Amineliass007@users.noreply.github.com>
2022-11-10 00:17:17 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,62 @@
+import requests
+import json
+from flask import Flask
+from flask import jsonify
+app = Flask("app")
+
+def getApiResponse(url):
+    auth="x-key"
+    value="2056efd4b2a3c1b2a25ee38794c9b118"
+    my_headers = {auth: value}
+    response = requests.get(url, headers=my_headers)
+    return(response.json())
+@app.route("/agsi/providers")
+def getAgsiProviders():    
+    url="https://agsi.gie.eu/api/about?listing=true"
+    
+    data=getApiResponse(url)
+    st={}
+    Li=[]
+    for country in data["SSO"].keys():
+        d={}
+        l=[]
+        d["country"]=country
+        for i in data["SSO"][country]:
+            s={}
+            s["zone"]=i["data"]["code"]
+            s["countryCode"]=i["data"]["country"]["code"]
+            s["countryName"]=i["data"]["country"]["name"]
+            s["companyName"]=i["name"]
+            s["companyEic"]=i["eic"]
+            s["companyType"]=i["data"]["type"]
+            for j in i["facilities"]:
+                s["facilityName"]=j["name"]
+                s["facilityEic"]=j["eic"]
+                s["facilityCountryName"]=j["country"]["name"]
+                s["facilityType"]=j["type"]
+            
+            l.append(s)
+        d["data"]=l
+        Li.append(d)
+    st["body"]=Li
+    return(jsonify(st))
+app.run(host='0.0.0.0', port=5000)
+
+
+@app.route("/agsi/facilities")
+def getAgsiFacilities():    
+    data=getAgsiProviders().json
+    Li=[]
+    for country in data["body"]:
+        d={}
+        for facility in country["data"]:
+            urlFacility="https://agsi.gie.eu/api?country="+str(facility["countryCode"])+"&company="+str(facility["companyEic"])+"&facility="+str(facility["facilityEic"])
+            data=getApiResponse(urlFacility)
+            for facilityData in data["data"]:
+                d["gas_day"]=data["gas_day"]
+                d["name"]=facilityData["name"]
+                d["gasDayStart"]=facilityData["gasDayStart"]
+                d["gasInStorage"]=facilityData["gasInStorage"]
+                Li.append(d)
+    return (jsonify(Li))
+#git
