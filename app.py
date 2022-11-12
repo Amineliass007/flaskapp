139e6b29acda77f597fc08fb4e36052c35de6694
Amineliass007 <115976127+Amineliass007@users.noreply.github.com>
2022-11-12 17:40:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return ("Hello world")
-app.set("port",3000)
 if __name__ == '__main__':
-app.run()
+    app.run()
 '''
 def getApiResponse(url):
     auth="x-key"
