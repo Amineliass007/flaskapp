a34347557c1ae85778b8b885eb1e3f0780c09ef9
Amineliass007 <115976127+Amineliass007@users.noreply.github.com>
2022-11-12 17:12:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import requests
 import json
 from flask import Flask
 from flask import jsonify
-app = Flask("app")
+app = Flask(__name__)
 @app.route("/")
 def hello():
-    return (jsonify("Hello world"))
+    return ("Hello world")
+
+if __name__ == '__main__':
+app.run()
 '''
 def getApiResponse(url):
     auth="x-key"
